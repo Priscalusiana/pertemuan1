c671a31db8068d0c9c60a621ec4bcb57aab84da5
Priscalusiana <145738381+Priscalusiana@users.noreply.github.com>
2023-10-13 19:45:31 +0700

    Delete Pertemuan_2 directory
    delete file

--- Pertemuan_2/hitungLuasLingkaran.java
+++
@@ -1,9 +0,0 @@
-package Pertemuan_2;
-public class hitungLuasLingkaran {
-    public static void main(String[] args) {
-        luasLingkaran L = new luasLingkaran();
-        System.out.println("Luas Lingkaran : " + L.hitungLuasL());
-        
-    }
-    
-}
