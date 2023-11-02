fd4baed937bb326c9002f57febe418291d5209df
Priscalusiana <145738381+Priscalusiana@users.noreply.github.com>
2023-11-02 20:40:36 +0700

    Add files via upload

---
+++ pertemuan5/src/konsep_overriding/mobil_matic.java
@@ -0,0 +1,14 @@
+
+package konsep_overriding;
+
+    public class mobil_matic extends mobil {
+   
+      @Override
+      public void jalankan (){
+      
+      System.out.println("Gigi diposisi D, Tekan gas ... ");
+  }
+    
+}
+
+
---
+++ pertemuan5/src/tugaspertemuan4dan5/wanita.java
@@ -0,0 +1,13 @@
+
+package tugaspertemuan4dan5;
+
+public class wanita extends manusia{
+    
+    @Override
+    public void berpakaian(){
+        
+        System.out.println("Wanita memakai jilbab dan rok panjang");
+    }
+    
+    
+}
